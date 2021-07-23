ae1bf47c7a0b29b36bc9afccfadf4be56be967e8
sky <854323752@qq.com>
2021-07-23 16:42:21 +0800

    feat: 二分查找法

--- packages/tools/you-need-suggest/src/levenshteinDistance.ts
+++ packages/tools/you-need-suggest/src/levenshteinDistance.ts
@@ -1,21 +1,4 @@
-export type TResult = {
-  continuous: number
-  count: number
-  position: number
-  distance: number
-}
-
-export interface IResultWrapper {
-  get(): TResult
-  setContinuous(continuous: number): void
-  getContinuous(): number
-  setCount(count: number): void
-  getCount(): number
-  setPosition(position: number): void
-  getPosition(): number
-  setDistance(distance: number): void
-  getDistance(): number
-}
+import { IResultWrapper, TResult } from '../typings.d'
 
 class ResultWrapper implements IResultWrapper {
   private continuous
