acfabd6e920846323f73b64ea0e5770a6cb8102b
sangbaipi <sangbaipi@2dfire.com>
2022-11-13 23:10:25 +0800

    fix: 添加figure

---
+++ packages/UtilShared/src/popular/figure.ts
@@ -0,0 +1,27 @@
+import TypeChecker from "@lihzsky/type-checker"
+
+export const Figure = {
+  toYuan(value: any): string {
+    if (TypeChecker.isNumber(value)) {
+      return (value / 100).toFixed(2)
+    }
+
+    return '0.00'
+  },
+
+  toFen(value: any): number {
+    if (TypeChecker.isString(value) && /^\d+(?:\.\d+)?$/.test(value)) {
+      return Number(value) * 100
+    }
+
+    return 0
+  },
+
+  toPercent(value: any): string {
+    if (TypeChecker.isNumber(value)) {
+      return (value / 100) + '%'
+    }
+
+    return ''
+  }
+}
